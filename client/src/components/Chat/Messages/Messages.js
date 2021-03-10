7f87e574b58f64335abdb9214d841f40f91b2636
Michael Li <hl5@ualberta.ca>
2021-03-10 12:28:43 -0700

    change scroll method, refactor

--- client/src/components/Chat/Messages/Messages.js
+++ client/src/components/Chat/Messages/Messages.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./Message/Message";
-import ScrollToBottom from "react-scroll-to-bottom";
 import "./Messages.css";
 export default function Messages(props) {
-    const { messages, name } = props;
-
-    return (
-        <ScrollToBottom className="messages">
-            {messages.map((message, index) => {
-                return (
-                    <div className="msg-box" key={index}>
-                        <Message message={message} name={name} />
-                    </div>
-                );
-            })}
-        </ScrollToBottom>
-    );
+	const { messages, name } = props;
+	const scrollRef = useRef();
+	useEffect(() => {
+		if (scrollRef.current) {
+			scrollRef.current.scrollIntoView({ behavior: "smooth" });
+		}
+	}, [messages]);
+	return (
+		<div className="messages">
+			{messages.map((message, index) => {
+				return (
+					<div className="msg-box" key={index}>
+						<Message message={message} name={name} />
+					</div>
+				);
+			})}
+			<div ref={scrollRef}></div>
+		</div>
+	);
 }
